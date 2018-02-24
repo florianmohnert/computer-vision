c37bb13a489d09613ff4e52a79c934b1d685b1d8
Procope <m.giulianelli.m@gmail.com>
2018-02-24 10:57:19 +0100

    Gaussian filters

--- Assignment_2/Part-1: Image Enhancement/gauss2D.m
+++ Assignment_2/Part-1: Image Enhancement/gauss2D.m
@@ -1,3 +1,11 @@
-function G = gauss2D( sigma , kernel_size )
+function G = gauss2D(sigma, kernel_size)
     %% solution
+    if mod(kernel_size, 2) == 0
+        error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
+    end
+    
+    G_x = gauss1D(sigma, kernel_size);
+    G_y = gauss1D(sigma, kernel_size);
+    
+    G = G_x' * G_y;
 end
--- Assignment_2/Part-1: Image Enhancement/slow_gauss2D.m
+++ Assignment_2/Part-1: Image Enhancement/slow_gauss2D.m
@@ -1,12 +1,12 @@
-function G = gauss1D(sigma, kernel_size)
+function G = slow_gauss2D(sigma, kernel_size)
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
 
     %% solution
     N = floor(kernel_size / 2);
-    x = linspace(-N, N, kernel_size);
+    [x, y] = meshgrid(-N:N, -N:N);
     
-    G = exp( -x .^ 2 / (2 * sigma^2) ) / sigma * sqrt(2*pi);
-    G = G ./ sum(G);  % normalise
+    G = exp( -(x .^ 2 + y .^ 2) / (2 * sigma^2) ) / sigma^2 * sqrt(2*pi);
+    G = G ./ sum(G(:));  % normalise
 end
