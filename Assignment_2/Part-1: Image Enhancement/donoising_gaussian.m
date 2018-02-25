0c871fb379f7033846f41b97fdb6ec6ec2f78ecd
Florian <mail@florian-mohnert.de>
2018-02-25 12:49:19 +0100

    implement compute_LoG

--- Assignment_2/Part-1: Image Enhancement/donoising_gaussian.m
+++ Assignment_2/Part-1: Image Enhancement/donoising_gaussian.m
@@ -1,10 +1,10 @@
 %% plots gaussian filter
 
 denoised_gaussian_05 = denoise(im2,'gaussian', 3, 0.5);
-denoised_gaussian_1 = denoise(im2,'gaussian', 3, 1);
-denoised_gaussian_2 = denoise(im2,'gaussian', 3, 2);
+denoised_gaussian_1 = denoise(im2,'gaussian', 5, 1);
+denoised_gaussian_2 = denoise(im2,'gaussian', 5, 2);
 
-figure(1)
+figure(3)
 subplot(1,3,1)
 p1 = imshow(denoised_gaussian_05);
 title('3x3 with sigma = 0.5');
@@ -16,7 +16,7 @@ p2 = imshow(denoised_gaussian_2);
 title('11x11 with sigma = 2');
 
 %% PSNR
-myPSNR(im2,denoised_gaussian_05)
-myPSNR(im2,denoised_gaussian_1)
-myPSNR(im2,denoised_gaussian_2)
+myPSNR(im1_original,denoised_gaussian_05)
+myPSNR(im1_original,denoised_gaussian_1)
+myPSNR(im1_original,denoised_gaussian_2)
 
--- Assignment_2/Part-1: Image Enhancement/plots_denoising.m
+++ Assignment_2/Part-1: Image Enhancement/plots_denoising.m
@@ -2,6 +2,7 @@
 %%
 im1 = imread('images/image1_saltpepper.jpg');
 im2 = imread('images/image1_gaussian.jpg');
+im1_original = imread('images/image1.jpg');
 %% saltpepper
 figure(1)
 denoised_box_3 = denoise(im1,'box', 3);
@@ -31,13 +32,12 @@ title('median 7x7');
 
 %%  PSNR 
 disp('image 1')
-psnr_box3_im1 = myPSNR(im1,denoised_box_3);
-psnr_box5_im1 = myPSNR(im1,denoised_box_5);
-psnr_box7_im1 = myPSNR(im1,denoised_box_7);
-psnr_median3_im1 = myPSNR(im1,denoised_median_3);
-psnr_median5_im1 = myPSNR(im1,denoised_median_5);
-psnr_median7_im1 = myPSNR(im1,denoised_median_7);
-
+psnr_box3_im1 = myPSNR(im1_original,denoised_box_3)
+psnr_box5_im1 = myPSNR(im1_original,denoised_box_5)
+psnr_box7_im1 = myPSNR(im1_original,denoised_box_7)
+psnr_median3_im1 = myPSNR(im1_original,denoised_median_3)
+psnr_median5_im1 = myPSNR(im1_original,denoised_median_5)
+psnr_median7_im1 = myPSNR(im1_original,denoised_median_7)
 %% gaussian
 figure(2)
 denoised_box_3 = denoise(im2,'box', 3);
@@ -67,10 +67,10 @@ title('median 7x7');
 
 %%
 disp('image2')
-psnr_box3_im2 = myPSNR(im2,denoised_box_3);
-psnr_box5_im2 = myPSNR(im2,denoised_box_5);
-psnr_box7_im2 = myPSNR(im2,denoised_box_7);
-psnr_median3_im2 = myPSNR(im2,denoised_median_3);
-psnr_median5_im2 = myPSNR(im2,denoised_median_5);
-psnr_median7_im2 = myPSNR(im2,denoised_median_7);
+psnr_box3_im2 = myPSNR(im1_original,denoised_box_3)
+psnr_box5_im2 = myPSNR(im1_original,denoised_box_5)
+psnr_box7_im2 = myPSNR(im1_original,denoised_box_7)
+psnr_median3_im2 = myPSNR(im1_original,denoised_median_3)
+psnr_median5_im2 = myPSNR(im1_original,denoised_median_5)
+psnr_median7_im2 = myPSNR(im1_original,denoised_median_7)
 
