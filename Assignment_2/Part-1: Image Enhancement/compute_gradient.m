b66ee7059415dca689562af1626a533265dcdd21
Procope <m.giulianelli.m@gmail.com>
2018-02-25 18:41:42 +0100

    Refactor

--- Assignment_2/Part-1: Image Enhancement/compute_gradient.m
+++ Assignment_2/Part-1: Image Enhancement/compute_gradient.m
@@ -1,6 +1,14 @@
 function [Gx, Gy, im_magnitude, im_direction] = compute_gradient(image)
 
-[Gx, Gy, im_magnitude, im_direction] = compute_gradient_(image, 0);
+i = double(image);
+sobel_x = [1 0 -1; 2 0 -2; 1 0 -1];
+sobel_y = [1 2 1; 0 0 0; -1 -2 -1];
+
+Gx = imfilter(i, sobel_x);
+Gy = imfilter(i, sobel_y);
+
+im_magnitude = sqrt(Gx .^ 2 + Gy .^ 2);
+im_direction = atan(Gy ./ Gx);
 
 end
 
