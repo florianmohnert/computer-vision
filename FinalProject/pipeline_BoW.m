17e6a24ee914161e073d227d74d72fbe4606b432
Procope <m.giulianelli.m@gmail.com>
2018-03-17 11:43:30 +0100

    Optimise image loading

--- FinalProject/pipeline_BoW.m
+++ FinalProject/pipeline_BoW.m
@@ -7,25 +7,29 @@ train_set_size = 2;
 
 
 %% Build the vocabulary
-images_airplanes = load_image_stack('Caltech4/ImageData/airplanes_train', sample_size);
-images_cars = load_image_stack('Caltech4/ImageData/cars_train', sample_size);
-images_faces = load_image_stack('Caltech4/ImageData/faces_train', sample_size);
-images_motorbikes = load_image_stack('Caltech4/ImageData/motorbikes_train', sample_size);
+images_airplanes = load_image_stack('Caltech4/ImageData/airplanes_train', sample_size + train_set_size);
+images_cars = load_image_stack('Caltech4/ImageData/cars_train', sample_size + train_set_size);
+images_faces = load_image_stack('Caltech4/ImageData/faces_train', sample_size + train_set_size);
+images_motorbikes = load_image_stack('Caltech4/ImageData/motorbikes_train', sample_size + train_set_size);
 
-images = [images_airplanes, images_cars, images_faces, images_motorbikes];
+images_vocab_building = [images_airplanes(1:sample_size), ...
+                         images_cars(1:sample_size),      ...
+                         images_faces(1:sample_size),     ...
+                         images_motorbikes(1:sample_size) ...
+                        ];
 
-descriptors = sift_descriptors(images, colorspace, detector);
+descriptors = sift_descriptors(images_vocab_building, colorspace, detector);
 descriptors = normc(double(descriptors));  % normalize descriptors
 
 [~, centroids] = kmeans(descriptors, vocab_size);
 
 
 %% Create image features
-images_airplanes = load_image_stack('Caltech4/ImageData/airplanes_train', train_set_size);
-images_cars = load_image_stack('Caltech4/ImageData/cars_train', train_set_size);
-images_faces = load_image_stack('Caltech4/ImageData/faces_train', train_set_size);
-images_motorbikes = load_image_stack('Caltech4/ImageData/motorbikes_train', train_set_size);
 
-images = [images_airplanes, images_cars, images_faces, images_motorbikes];
+images_train = [images_airplanes(sample_size+1:end), ...
+                images_cars(sample_size+1:end),      ...
+                images_faces(sample_size+1:end),     ...
+                images_motorbikes(sample_size+1:end) ...
+               ];
 
-[histograms] = histograms_of_words(images, centroids, colorspace, detector);
+[histograms] = histograms_of_words(images_train, centroids, colorspace, detector);
