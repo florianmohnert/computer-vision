9b6cf9c9228b963ae42b6413d1bccac2bc4162c2
Procope <m.giulianelli.m@gmail.com>
2018-03-17 11:22:12 +0100

    Add comments

--- FinalProject/load_image_stack.m
+++ FinalProject/load_image_stack.m
@@ -1,29 +1,37 @@
 function [image_stack] = load_image_stack(dir_path, n)
+%
+% Args:
+%   dir_path: path of image dataset
+%   n:        number of images to load
+%
+% Return: a cell array of n images
 
-    files = dir(dir_path);
-    n_sources = length(files); % the number of images
-        
-    image_stack = {};
-   
-    random_indices = randperm(n_sources);
-    cnt = 0;
-    for idx = 1:length(random_indices)
+files = dir(dir_path);
+n_sources = length(files); % the number of images
+
+image_stack = {};
+
+random_indices = randperm(n_sources);
+img_count = 0;
+
+for idx = 1:length(random_indices)
     
-        f_name = files(random_indices(idx)).name;
-        
-        if (length(f_name) < 4) || (f_name(end-3:end) ~= ".jpg")
-            continue;
-        end 
-            
-        I = imread(strcat(files(random_indices(idx)).folder, '/', f_name));
-        
-        if ndims(I) == 3
-            image_stack{idx} = I;
-            cnt = cnt + 1;
-        end
-        
-        if cnt == n  break;  end
-        
-        
+    f_name = files(random_indices(idx)).name;
+    
+    % ignore everything but images
+    if (length(f_name) < 4) || (f_name(end-3:end) ~= ".jpg")
+        continue;
     end
+    
+    I = imread(strcat(files(random_indices(idx)).folder, '/', f_name));
+    
+    % TODO: we only load RGB images for now
+    if ndims(I) == 3
+        image_stack{idx} = I;
+        img_count = img_count + 1;
+    end
+    
+    % stop when n images have been loaded
+    if img_count == n  break;  end
+end
 end
--- FinalProject/sift_descriptors.m
+++ FinalProject/sift_descriptors.m
@@ -1,27 +1,27 @@
 
-function [descriptors] = sift_descriptors(images,colorspace,feature_detector)
+function [descriptors] = sift_descriptors(images, colorspace, feature_detector)
 %%%%%%%%%%%%%
-% images: cell of images
-% colorspace: string "RGB","rgb","opponent"
-% feature_detector: "dense", "keypoints"
+% images:           cell of images
+% colorspace:       "RGB", "rgb", or "opponent"
+% feature_detector: "dense" or "keypoints"
 %%%%%%%%%%%%%
 
 descriptors_all = [];
 
 for i = 1:length(images)
-    
+    % extract image from cell array
     im = im2single(cell2mat(images(i)));
     
     if (colorspace == 'RGB')
-        descriptors = sift3d(im,feature_detector);
-        
+        descriptors = sift3d(im, feature_detector);
+   
     elseif (colorspace == 'rgb')
         im = RGB2rgb(im);
-        descriptors = sift3d(im,feature_detector);
+        descriptors = sift3d(im, feature_detector);
         
     elseif (colorspace == 'opponent')
         im = rgb2opponent(im);
-        descriptors = sift3d(im,feature_detector);
+        descriptors = sift3d(im, feature_detector);
     end   
     
     descriptors_all = cat(1, descriptors_all, descriptors);
